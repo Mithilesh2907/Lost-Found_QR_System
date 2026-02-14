96cf1e636335e35a35a7ab357ffd7f7db81d6b01
Mithilesh Sawant <mithileshsawant2907@gmail.com>
2026-02-14 20:39:30 +0530

    updated ScanHistoryRepository

--- src/main/java/com/example/lostfound/repository/ScanHistoryRepository.java
+++ src/main/java/com/example/lostfound/repository/ScanHistoryRepository.java
@@ -3,5 +3,8 @@ package com.example.lostfound.repository;
 import com.example.lostfound.entity.ScanHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScanHistoryRepository extends JpaRepository<ScanHistory, Long> {
+    List<ScanHistory> findByTag_IdOrderByTimestampDesc(long TagId);
 }
